73fed578b69eea20172fdbd390fc811c0ce5d365
eks9527 <53614568+eks9527@users.noreply.github.com>
2021-01-26 21:23:36 +0800

    Update app.py

--- Flask/app.py
+++ Flask/app.py
@@ -6,7 +6,7 @@ from flask_pymongo import PyMongo
 
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://eks210017:eks210017@cluster0.yrvuu.mongodb.net/mydatabase?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = ""
 mongo = PyMongo(app)
 total = 0
 
